c1796ac0017e736b56bdc7adb78f7c04045750fa
yakummi <GorobtsovIvan@mail.ru>
2023-04-04 00:19:18 +0300

    step-6

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ from main import Operator
 
 test_example = Operator(4, 2)
 def test_one():
-    assert test_example.adding() == 6
+    assert test_example.adding() == 5
 
 def test_two():
     assert test_example.subtracting() == 2
